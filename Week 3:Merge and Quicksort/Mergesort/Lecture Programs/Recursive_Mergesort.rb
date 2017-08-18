b61303df25761c2476afe61ecca000770db3d4a2
Devansh Patel <pateldd@goldmail.etsu.edu>
2017-08-18 14:44:38 -0700

    fix mergesort

--- Week 3:Merge and Quicksort/Mergesort/Lecture Programs/Recursive_Mergesort.rb
+++ Week 3:Merge and Quicksort/Mergesort/Lecture Programs/Recursive_Mergesort.rb
@@ -1,4 +1,4 @@
-def sort(arr)
+def mergesort(arr)
   hi = arr.size - 1
   lo = 0
   mid = (lo + hi)/2
@@ -11,8 +11,8 @@ def sort(arr)
     end
     return arr
   end
-  half1 = sort(arr[lo..mid])
-  half2 = sort(arr[mid+1..hi])
+  half1 = mergesort(arr[lo..mid])
+  half2 = mergesort(arr[mid+1..hi])
   if half1[-1] <= half2[0]
     return half1+half2
   else
@@ -22,27 +22,32 @@ end
 
 def merge(arr1,arr2)
   aux = []
-  until arr1.empty? && arr2.empty?
-    a = arr1[0]
-    b = arr2[0]
-    if arr1.empty?
-      aux << arr2.shift
-    elsif arr2.empty?
-      aux << arr1.shift
-    elsif a < b
-      aux << arr1.shift
+  i = 0
+  j = 0
+  until i >= arr1.length && j >= arr2.length
+    a = arr1[i]
+    b = arr2[j]
+    if i >= arr1.length
+      aux << b
+      j += 1
+    elsif j >= arr2.length
+      aux << a
+      i += 1
+    elsif a <= b
+      aux << a
+      i += 1
     elsif b < a
-      aux << arr2.shift
-    elsif a == b
-      aux << arr1.shift
+      aux << b
+      j += 1
     end
   end
   aux
 end
+
 a = (1..1000).to_a.shuffle
-d = Time.now
-100.times do
-sort(a)
-end
-b = Time.now
-puts ((b-d)*1000)/100
+# d = Time.now
+# 100.times do
+p mergesort(a)
+# end
+# b = Time.now
+# puts ((b-d)*1000)/100
