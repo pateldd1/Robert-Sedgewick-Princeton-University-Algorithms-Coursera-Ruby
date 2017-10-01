55790420be53b1dbeb6aeff8e064793417f98a29
Devansh Patel <pateldd@goldmail.etsu.edu>
2017-09-30 20:03:26 -0700

    change 4SUM

--- Week 6:Hash Tables/Hash Tables/Interview Questions/4sum in O(n^2).rb
+++ Week 6:Hash Tables/Hash Tables/Interview Questions/4sum in O(n^2).rb
@@ -1,29 +1,20 @@
-# 4-SUM. Given an array a[] of n integers, the 4-SUM problem is to determine 
-# if there exist distinct indices i, j, k, and l such that a[i]+a[j]=a[k]+a[l].
-#   Design an algorithm for the 4-SUM problem that takes time proportional to n2
-#   (under suitable technical assumptions).
+# 4-SUM PROBLEM
 
-def foursum(arr)
-  hash = Hash.new
-  twos = arr.combination(2).to_a
-  two_sums = twos.map{|subarr| subarr.reduce(:+)}
-  two_sums.each_with_index do |sum,inx|
-    hash[sum] = [] if !hash[sum]
-    hash[sum] << twos[inx] unless hash[sum].include?(twos[inx])
-  end
-  answer = []
-  hash.each do |k,v|
-    if hash[k].size > 1
-      answer << hash[k].combination(2).to_a
+def four_sum(nums, target)
+    hash = Hash.new(){|h,k| h[k] = []}
+    combos = (0...nums.length).to_a.combination(2).to_a
+    combos.each do |a,b|
+        hash[nums[a]+nums[b]].push([a,b])
     end
-  end
-  answer.flatten(1)
+    ans = []
+    hash.keys.each do |key|
+        if hash[target-key].length > 0
+            addon = hash[key].product(hash[target-key])
+            addon.map!(&:flatten).map!(&:uniq).reject! {|subarr| subarr.length < 4}
+            ans += addon
+        end
+    end
+    ans.map do |a,b,c,d|
+        [nums[a],nums[b],nums[c],nums[d]].sort
+    end.uniq
 end
-arr= []
-160.times do
-  arr << rand(1..160)
-end
-x = Time.now
-foursum(arr)
-y = Time.now
-p (y-x)*1000
